725e8e255885aa356840e05837e98e7a6b50c440
Shashi Kumar <shashikumar942@gmail.com>
2024-07-19 22:47:38 +0530

    prisma ORM setup

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import express from 'express';
-import { title } from 'process';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const init = async () => {
     const app = express();
+    // create graphql server
     const server = new ApolloServer({
         typeDefs: `
         type Todo {
@@ -17,15 +20,40 @@ const init = async () => {
             sayHello: String,
             getTodos: [Todo]
         }
+        type User {
+            id: ID!
+            name: String
+            email: String!
+        }
+        type Mutation {
+            createUser(name: String, email: String!): String,
+            getUsers: [User]
+        }
         `,
         resolvers: {
             Query: {
                 sayHello: () => 'Hello from GraphQL',
                 getTodos: () => [{ id: 1, name: 'qwert', title: 'qwerty' }]
+            },
+            Mutation: {
+                createUser: async (_parent, { name, email }: { name: string, email: string }) => {
+                    await prisma.user.create({
+                        data: {
+                            name,
+                            email
+                        }
+                    })
+                    return 'User created';
+                },
+                getUsers: async () => {
+                    const users = await prisma.user.findMany();
+                    return users || [];
+                }
             }
         },
     });
 
+    // start graphql server
     await server.start();
 
     app.use(cors());
